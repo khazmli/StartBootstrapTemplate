a9b7ce78f638571c9f0a517d523b78a40a18c38e
khazmli <togrul.dj@gmail.com>
2024-03-14 20:05:35 +0300

    v1

---
+++ src/script/main.js
@@ -0,0 +1,49 @@
+
+// let body=document.querySelector("body")
+
+
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//   if (body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementsByClassName("navbar")[0].style.backgroundColor= "white";
+//     let li = Array.from(document.querySelectorAll("li"))
+//     let list =  li.map(item=>{
+//     console.log(item.children[0])
+//         return item.children[0]
+//     })
+// li.style.color="red"
+//   } else {
+//     document.getElementsByClassName("navbar")[0].style.backgroundColor = "transparent";
+//     document.getElementsByClassName("navbar")[0].style.color= "white";
+//   }
+// }
+
+
+
+
+// let body = document.querySelector("body");
+
+// window.onscroll = function() {
+//     scrollFunction();
+// };
+
+// function scrollFunction() {
+//     if (body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         let navbar = document.getElementsByClassName("navbar");
+//         for (let i = 0; i < navbar.length; i++) {
+//             navbar[i].style.backgroundColor = "white";
+//             navbar[i].style.color = "black"; 
+//         }
+
+//         let li = document.querySelectorAll(".navbar li");
+//         li.forEach(item => {
+//             item.style.color = "black"; 
+//         });
+//     } else {
+//         let navbar = document.getElementsByClassName("navbar");
+//         for (let i = 0; i < navbar.length; i++) {
+//             navbar[i].style.backgroundColor = "transparent";
+//             navbar[i].style.color = "white";
+//         }
+//     }
+// }
